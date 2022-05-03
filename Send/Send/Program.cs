85fa93279625878e42bf73bbc1d414268f00ac70
Aldo Luiz <aldoluiz21@gmail.com>
2022-05-02 23:47:53 -0300

    first commit

---
+++ Send/Send/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("escreve uma mensagem para enviar para a fila");
+var text = Console.ReadLine();  
+
+Send.Send.Main(text);
+
---
+++ Send/Send/Send.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Send
+{
+    class Send
+    {
+        public static void Main(string text)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+               
+                var body = Encoding.UTF8.GetBytes(text);
+
+                channel.BasicPublish(exchange: "",
+                                      routingKey: "hello",
+                                      basicProperties: null,
+                                      body: body);
+
+                Console.WriteLine("[x] sent {0}", text);
+            }
+
+            Console.WriteLine(" Press [enter] to exit");
+            Console.ReadLine(); 
+            
+        }
+    }
+}
